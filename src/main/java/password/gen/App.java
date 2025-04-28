15062151be21df956ed4820abd8d0fe53303018d
Arthur Andreev <arthwr04@gmail.com>
2025-04-28 21:48:48 +0300

    update: validate inputs and catch errors

--- src/main/java/password/gen/App.java
+++ src/main/java/password/gen/App.java
@@ -4,19 +4,84 @@ import java.util.Scanner;
 
 public class App 
 {
+
+    private static Scanner scanner = new Scanner(System.in);
     public static void main( String[] args ){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the length of the password:");
-        int length = scanner.nextInt();
-        System.out.println("Include uppercase letters? (true/false):");
-        boolean useUppercase = scanner.nextBoolean();
-        System.out.println("Include lowercase letters? (true/false):");
-        boolean useLowercase = scanner.nextBoolean();
-        System.out.println("Include numbers? (true/false):");
-        boolean useNumbers = scanner.nextBoolean();
-        System.out.println("Include special characters? (true/false):");
-        boolean useSpecialChars = scanner.nextBoolean();
-        String pass = password.generate(length, useUppercase, useLowercase, useNumbers, useSpecialChars);
-        System.out.println("Generated password: " + pass);
+        boolean flag = true;
+        while(flag){
+           showMenu();
+            int choice = scanner.nextInt();
+            switch(choice){
+                case 1:
+                    String pass = password.generate(askLength(), askUppercase(), askLowercase(), askNumbers(), askSpecialChars());
+                    System.out.println("Generated Password: " + pass);
+                    break;
+                case 2:
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
+
+    public static void showMenu() {
+        System.out.println("Welcome to Password Generator");
+        System.out.println("1. Generate Password");
+        System.out.println("2. Exit");
+        System.out.print("Enter your choice: ");
+    }
+
+    public static int askLength() {
+        System.out.print("Enter the length of the password: ");
+        int length = scanner.nextInt();
+        if(length <= 1) {
+            System.out.println("Length must be at least 1.");
+            askLength();
+        }
+        return length;
+    }
+    public static boolean askUppercase() {
+        System.out.print("Include uppercase letters? (true/false): ");
+        if(scanner.hasNextBoolean()) {
+            boolean useUppercase = scanner.nextBoolean();
+            return useUppercase;
+        } else {
+            System.out.println("Invalid input. Please enter true or false.");
+            scanner.next();
+            askUppercase();
+        }
+        return false;
+    }
+    public static boolean askLowercase() {
+        System.out.print("Include lowercase letters? (true/false): ");
+        if(scanner.hasNextBoolean()) {
+            return scanner.nextBoolean();
+        } else {
+            System.out.println("Invalid input. Please enter true or false.");
+            scanner.next(); 
+            return askLowercase();
+        }
+    }
+    public static boolean askNumbers() {
+        System.out.print("Include numbers? (true/false): ");
+        if(scanner.hasNextBoolean()) {
+            return scanner.nextBoolean();
+        } else {
+            System.out.println("Invalid input. Please enter true or false.");
+            scanner.next();
+            return askNumbers();
+        }
+    }
+    public static boolean askSpecialChars() {
+        System.out.print("Include special characters? (true/false): ");
+        if(scanner.hasNextBoolean()) {
+            return scanner.nextBoolean();
+        } else {
+            System.out.println("Invalid input. Please enter true or false.");
+            scanner.next();
+            return askSpecialChars();
+        }
+    }
+
 }
